368840f2efdf8eb15a568762c425e91e10389b8e
sotiris9 <palamarcuks543@gmail.com>
2026-04-25 11:35:06 +0300

    add config for sheets and time

---
+++ api/config.js
@@ -0,0 +1,70 @@
+// api/config.js
+
+module.exports = {
+  // Налаштування твоїх Google Таблиць
+  mon: {
+    id: "1lok-vuNC6Nx_Dx4w2vhRy8bnR0A6ssq2WUXtClGWj9Q",
+    sheets: {
+      it: "1778922595",
+      finance: "325629102",
+      enterprise: "1587751514",
+      audience: "436522941",
+    },
+  },
+  tue: {
+    id: "10UugoyVXw4mwzgFjqO6pnr1v5ofPDQRdjE8NGy_fVRQ",
+    sheets: {
+      it: "1778922595",
+      finance: "325629102",
+      enterprise: "1587751514",
+      audience: "436522941",
+    },
+  },
+  wed: {
+    id: "1VvEML21gmiHdYIMB2aq-B9Ea7n8w_F9YrtTsz5mtq50",
+    sheets: {
+      it: "1778922595",
+      finance: "325629102",
+      enterprise: "1587751514",
+      audience: "436522941",
+    },
+  },
+  thu: {
+    id: "1zPrelCai8jGVcZMREDGltl8yIpLGXqr_288uTwtjVG0",
+    sheets: {
+      it: "1778922595",
+      finance: "325629102",
+      enterprise: "1587751514",
+      audience: "436522941",
+    },
+  },
+  fri: {
+    id: "1I0TjCHqnEwaNFQrTaj86z_iII-7i_Xl9s7JiIupEURo",
+    sheets: {
+      it: "1778922595",
+      finance: "325629102",
+      enterprise: "1587751514",
+      audience: "436522941",
+    },
+  },
+  sat: {
+    id: "1Uk4LNAHU22luWeAYIidY5jQ2N5dyGlUrwI2SFphQ3pc",
+    sheets: {
+      it: "1778922595",
+      finance: "325629102",
+      enterprise: "1587751514",
+      audience: "436522941",
+    },
+  },
+};
+
+  // Розклад дзвінків (один на весь проєкт)
+  timeMap: {
+    "1": "08:30 - 09:50",
+    "2": "10:00 - 11:20",
+    "3": "11:30 - 12:50",
+    "4": "13:30 - 14:50",
+    "5": "15:00 - 16:20",
+    "6": "16:30 - 17:50"
+  }
+};
